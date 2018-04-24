4518a8371d9ae6e86befd6935aa441a9ac06fc30
邓江洲 <dengjingzhou@zto.cn>
2018-04-24 11:30:56 +0800

    “Almost ; , ;

---
+++ web_pack/src/index.js
@@ -0,0 +1,8 @@
+console.log('I am entry point');
+
+const arr = [1 , 2 , 3];
+
+
+const iAmJavascriptES6 = () => console.log(...arr);
+
+window.iAmJavascriptES6 = iAmJavascriptES6;
---
+++ web_pack/webpack.config.js
@@ -0,0 +1,31 @@
+module.exports = {
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use:{
+                    loader: "babel-loader"
+                }
+
+
+
+
+
+            }
+
+
+
+
+
+        ]
+
+
+
+
+
+    }
+
+
+
+};
